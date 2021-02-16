5a7914c253fe54689e3b1cfbab21005367412ff5
David Taylor <david@taylorhq.com>
2021-02-16 10:33:20 +0000

    Display spinner after 2s of loading

--- javascripts/discourse/services/loading-indicator.js
+++ javascripts/discourse/services/loading-indicator.js
@@ -1,10 +1,12 @@
 import Service from "@ember/service";
 import Evented from "@ember/object/evented";
-import { schedule } from "@ember/runloop";
+import { schedule, later, cancel } from "@ember/runloop";
 
 const STORE_LOADING_TIMES = 5;
 const DEFAULT_LOADING_TIME = 0.3;
 const MIN_LOADING_TIME = 0.1;
+
+const STILL_LOADING_DURATION = 2;
 
 export default Service.extend(Evented, {
   init() {
@@ -25,14 +27,31 @@ export default Service.extend(Evented, {
         `${this.averageTime.toFixed(2)}s`
       );
     });
+
+    if (this.stillLoadingTimer) {
+      cancel(this.stillLoadingTimer);
+    }
+
+    this.stillLoadingTimer = later(
+      this,
+      "stillLoading",
+      STILL_LOADING_DURATION * 1000
+    );
+  },
+
+  stillLoading() {
+    schedule("afterRender", () => {
+      document.body.classList.add("still-loading");
+    });
   },
 
   end() {
     this.updateAverage((Date.now() - this.startedAt) / 1000);
     this.set("loading", false);
     this.trigger("stateChanged", false);
+    cancel(this.stillLoadingTimer);
     schedule("afterRender", () => {
-      document.body.classList.remove("loading");
+      document.body.classList.remove("loading", "still-loading");
     });
   },
 
