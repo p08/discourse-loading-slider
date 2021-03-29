36a6eb5379abd353f14da0a501a4dc877ec24106
P <prasen@rathore.com.au>
2021-03-29 13:59:27 +1030

    Update setup-loading-indicator.js

--- javascripts/discourse/api-initializers/setup-loading-indicator.js
+++ javascripts/discourse/api-initializers/setup-loading-indicator.js
@@ -7,8 +7,6 @@ import { set } from "@ember/object";
 import { isAppWebview } from "discourse/lib/utilities";
 
 export default apiInitializer("0.8", (api) => {
-  delete Ember.TEMPLATES["loading"];
-
   api.modifyClass("route:application", {
     loadingIndicator: service(),
 
@@ -19,11 +17,6 @@ export default apiInitializer("0.8", (api) => {
         this.loadingIndicator.end();
       });
 
-      let superLoading = this._super();
-      if (superLoading !== null) {
-        return superLoading;
-      }
-
       return true;
     },
   });
@@ -33,32 +26,7 @@ export default apiInitializer("0.8", (api) => {
     // component. This change should probably be made in core, but keeping it here for now.
     @observes("posts")
     _postsChanged() {
-      this._refresh();
-    },
-
-    // When refresh is called, the posts on screen might be different, and they might even belong
-    // to a different topic. Therefore we need to trigger _scrollTriggered to make sure the screen-track
-    // service is updated about the change.
-    _refresh(args) {
-      this._super(args);
-      this._scrollTriggered();
-    },
-  });
-
-  api.modifyClass("component:topic-list-item", {
-    // Core updates the header with topic information as soon as a topic-list-item is clicked
-    // This feels a little weird when the body is still showing old post content, so disable
-    // that behavior.
-    navigateToTopic(topic, href) {
-      // this.appEvents.trigger("header:update-topic", topic); // This is the core line we're removing
-      DiscourseURL.routeTo(href || topic.get("url"));
-      return false;
-    },
-  });
-
-  api.modifyClass("controller:discovery", {
-    set loading(value) {
-      // no-op. We don't want the loading spinner to show on the discovery routes any more
+      this.queueRerender();
     },
   });
 
